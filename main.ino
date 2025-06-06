9d9a103db76dae53c87ae7bb9babafb680552a12
Melchior <131868777+Melnando@users.noreply.github.com>
2025-06-06 11:20:49 +0200

    Update main.ino to original code

--- main.ino
+++ main.ino
@@ -1,39 +1,231 @@
-                               #include/**/<time.h>
-                               #include <ncurses.h>
-                               # include <stdlib.h>
-             /**               */#define O()for(y-=               !!\
-           y;y<H&&           /*semi-automatic----*/y<           p/W+2;\
-         y++)for(x=p%   W,x-=!!/*-----minesweeper*/x;x<W&&   x<p%W+2;x++)
-       #define _(x)COLOR_##x/*IOCCC2019IOCCC2019IOCCC2019IOCCC2019IOCCC20*/
-     #define Y(n)attrset(COLOR_PAIR(n)),mvprintw/*IOCCC2019IOCCC2019IOCCC20*/
-   int*M,W,H,S,C,E,X,T,c,p,q,i,j,k;char G[]=" x",U[256];int F(int p){int r=0,x,
-      y=p/W,q;O()q=y*W+x,r+=M[q]^=p-q?(M[q]&16)<<8:0;return r;}int K( int p,
-         int f,int g){int        x=(g+f/256)%16-(f+g/256)%16,y=p/W,c=0,n=
-          g/4096,m=x==n             ?0:x==g/16%16-f/16%16-n?256:-1;if(m+
-         1)O()if((M[n=               y*W+x]&4368)==4112)M[n]=(M[n]&~16)|m
-        ,c++;return c;               }void D(){int p,k,o=0,n=C;if(LINES-1<
-       H||COLS/2<W)/*|               */clear(),Y(4)(LINES/2,COLS/2-16,"Mak\
-e the terminal window "             "bigger!");else{for(p=0;p<S;o+=k==3,Y(k)(p/W+1
-,p%W*2,G),p++)G[1]="_*!.."       "12345678"[k=E?M[p]&256?n--,2:E-2||M[p]%2<1?M[p]&
-16?3:4+F(p)%16:1:3];k=T+time(0);T=o||T>=0||E-1?T:k;k=T<0?k:T;Y(7)(0,0,"%03d%*s%03\
-d",n>999?999:n,W*2-6,"",k>999?999:k);Y(9)(0,W-1,E>1?"X-(":E-1||o?":-)":"[-)");}re\
-fresh();}int main(int A,char**V){short B[]={_(RED),_(BLACK),_(WHITE),_(BLUE),_(GR\
-EEN),_(RED),_(MAGENTA),_(YELLOW),_(CYAN)};MEVENT e;FILE*f;srand(time(0));initscr()
-;for(start_color();X<12;X++)init_pair(X+1,B[X&&X<10?X-1:2],B[X?X<3?2:1:0]);noecho(
-);cbreak();timeout(9);curs_set(0);keypad(stdscr,TRUE);for(mousemask(BUTTON1_CLICK\
-ED|BUTTON1_RELEASED,0);;){if(A-->1){f=fopen(V[A],"r");fscanf(f,"%d %d %d",&W,&H,&C
-       );}else{f=0;W=COLS/2;H=LINES-1;C=W*H/5;}S=W*H;M=realloc(M,S*2*sizeof
-        (int));for(i=0;i<S;i++)!f?M[i]=i:fscanf(f,"%d",M+i);for(i=0;!f&&i<
-         S;i++)k=M[j=i+rand()%(S-i)],M[j]=M[i],M[i]=k;if(f)fclose(f);T=E=
-          X=0;for(clear();D(),c=getch(),c-'r'&&(c-KEY_RESIZE||E);){if (c
-         =='q')return(endwin(),0);if(c==KEY_MOUSE&&getmouse(&e)==OK&&e.x/
-      2<W&&e.y<=H){if(!e.y&&(W-2<e.x&&e.x<W+2))break;p=e.x/2+e.y*W-W;if(p>=0
-   ){if(!E){for(i=0;i<S;i++)M[S+M[i]]=i,M[i]=16+(M[i]<C);C-=M[p]&1;M[p]=16;E=1;
-     T=-time(0);}if(E<2)M[p]&=(M[p]&257)==1?T+=time(0),E=2,273:257;}}for(p=0;
-       p<S&&E==1;)M[p++]&=273;for(i=(X+S-1)%S;E==1&&i!=X;X=(X+1)%S){if(!(M[
-         p=M[X+S]]&272   )){if(K(p,c=F(p),0))goto N;for(k   =p/W-2,k=k<0?
-           0:k;k<H          &&k<p/W+3;k++)for(j=p%W-2           ,j=j<0?
-             0:j               ;j<W&&j<p%W+3;j++)if               (!(
-                               M[q=j+k*W]&272)){if(
-                               K(p,c,F(q)))goto N;F
-                               (q);}F(p);}}N:;}}}
+// ASSERVISSEMENT
+
+//pin utilisés moteur 1
+uint8_t enablePin1 = 9;
+uint8_t avancerPin1 = 10;
+uint8_t reculerPin1 = 8;
+
+//pin acquérant les ticks moteur 1
+uint8_t acquereur1 = 2;
+
+//comptage de ticks Moteur 1
+volatile uint8_t tick_codeur1 = 0;
+volatile uint8_t nbTicks1 = 0;
+
+//Nombre de mesures
+int mesures = 0;
+
+//Paramètres du moteur 1
+int cmd1 = 0;
+float rpm1 = 0;
+float erreur1 = 0;
+float consigneRPM1 = 130;
+float somme_erreur1 = 0;
+float delta_erreur1 = 0;
+float erreur_precedente1 = consigneRPM1;
+
+//correcteurs Moteur 1
+int kp1 = 50;
+int ki1 = 0;
+int kd1 = 0;
+
+void compteur1();
+void asservissement1(int nbTicks1);
+
+
+
+//Asservissement moteur 2
+
+//pin utilisés moteur 2
+uint8_t enablePin2 = 6;
+uint8_t avancerPin2 = 7;
+uint8_t reculerPin2 = 5;
+
+//pin acquérant les ticks moteur 2
+uint8_t acquereur2 = 3;
+
+//comptage de ticks Moteur 2
+volatile uint8_t tick_codeur2 = 0;
+volatile uint8_t nbTicks2 = 0;
+
+//Paramètres du moteur 2
+int cmd2 = 0;
+float rpm2 = 0;
+float erreur2 = 0;
+float consigneRPM2 = 130;
+float somme_erreur2 = 0;
+float delta_erreur2 = 0;
+float erreur_precedente2 = consigneRPM2;
+
+//correcteurs Moteur 2
+int kp2 = 50;
+int ki2 = 0;
+int kd2 = 0;
+
+void compteur2();
+void asservissement2(int nbTicks2);
+
+
+
+
+// CAPTEURS DISTANCE
+
+// initialisation des variables globales
+
+int trig = 13;
+int echo = 12; 
+int echo2 = 11; 
+int echo3 = 4;
+long lecture_echo;    // long correspond à une variable allant jusqu’à 32 bits
+long lecture_echo2;
+long lecture_echo3; 
+long cm; 
+long cm2;
+long cm3;
+//fonctions utilisées
+void distance_Capteurs();
+
+
+// FONCTIONS POUR LE DEPLACEMENT
+
+
+int marge = 12;
+
+//variables servant aux fonctions de déplacement
+int lent = 30;
+bool ralentit = false;
+bool arrete = true;
+bool accelere = false;
+bool recule = false;
+bool obstacle = false;
+int rapide = 130;
+
+void ralentir();
+void contournerG();
+void arreter();
+void reculer();
+
+
+
+void setup() {
+  
+// ASSERVISSEMENT
+
+// Setup des pins utilisés pour le moteur 1 
+pinMode(enablePin1,OUTPUT);
+pinMode(avancerPin1,OUTPUT);
+pinMode(reculerPin1,OUTPUT);
+
+
+digitalWrite(avancerPin1, LOW);
+digitalWrite(reculerPin1, LOW);
+
+
+// Setup des pins utilisés pour le moteur 2
+
+attachInterrupt(digitalPinToInterrupt(acquereur1),compteur1,RISING); //A chaque tick du reçu par acquereur1, cela déclenche la fonction compteur
+
+//Setup des pins utilisés pour le moteur 2
+pinMode(enablePin2,OUTPUT);
+pinMode(avancerPin2,OUTPUT);
+pinMode(reculerPin2,OUTPUT);
+
+
+digitalWrite(avancerPin2, LOW);
+digitalWrite(reculerPin2, LOW);
+
+
+attachInterrupt(digitalPinToInterrupt(acquereur2),compteur2,RISING);
+
+// CAPTEURS
+pinMode(trig, OUTPUT);  // le pin de valeur « trig » est configuré en sortie
+pinMode(echo, INPUT);   // le pin de valeur « echo » est configuré en entrée
+pinMode(echo2, INPUT); 
+pinMode(echo3, INPUT); 
+
+digitalWrite(trig, LOW);  // le pin de valeur « trig » est mis à l’état bas
+
+//Timers
+
+TCCR2A = 0;   //Mode normal(pas PWM) 
+TCCR2B = B111;  //prescaler 1024
+TIMSK2 = 0x01;  //interruption sur débordement du timer
+
+sei(); // interruptions activées
+
+Serial.begin(9600); //Communication avec le PC mise à 9600 bit/s
+analogWrite(enablePin1, 255);
+}
+
+void loop() {
+  distance_Capteurs(); //mesure de la distance par les capteurs
+
+  if(cm>12 && cm2>12&&cm3>12 && arrete == true&&obstacle == false){
+    avancer();  
+  }
+  else if((cm<12 || cm2<12 || cm3<12)){
+    arreter();
+    reculer();
+    while(cm<12 || cm2<12 || cm3<12){
+      distance_Capteurs();
+    }
+    obstacle = true;    
+  }
+  else if(cm3>cm && obstacle == true){ //évitement de l'obstacle par la droite
+    avancer();
+    arreter();
+    tournerD();
+    arreter();
+    avancer();
+    distance_Capteurs();
+    if(cm>12 && cm2>12 && cm3>12){
+      obstacle = false;
+    }
+  }
+  else if(cm3<cm && obstacle == true){ // évitement de l'obstacle par la gauche
+    avancer();
+    arreter();
+    tournerG();
+    arreter();
+    if(cm>12 && cm2>12 && cm3>12){
+      obstacle = false;
+    }
+  }
+
+// affichage
+      Serial.print("capteur 1 :");
+  Serial.println(cm);
+  Serial.print("capteur 2 :");
+  Serial.println(cm2);
+  Serial.print("capteur 3 :");
+  Serial.println(cm3);
+  Serial.print(rpm2,8);
+    Serial.print(" : ");
+    Serial.print(erreur2,4);
+    Serial.println();
+
+  
+  /*if(mesures >100){ //utilisé pour faire les mesures de la réponse indicielle des moteurs
+    analogWrite(enablePin1,0);
+    analogWrite(enablePin2,0);
+    cli();*/
+}
+
+ISR(TIMER2_OVF_vect){ // interruption toutes les 16,4 ms
+
+  
+
+//Comptage ticks moteur 1
+ nbTicks1 = tick_codeur1;
+ asservissement1(nbTicks1); // asservissement du moteur 1
+ tick_codeur1 = 0;
+
+//Comptage ticks moteur 2
+ nbTicks2 = tick_codeur2;
+ asservissement2(nbTicks2); // asservissement du moteur 2
+ tick_codeur2=0;
+  // start new ticks count. Compte le nombre de ticks durant l'interruption de periode T. 
+
+
+
+}
